96943f7bd642b29e0a36ae8845870e26d842bc5a
Tristan <tristanchay123@gmail.com>
2023-10-23 12:32:38 +0800

    Initial Commit

---
+++ Growcalth-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Growcalth-iOS
+//
+//  Created by Tristan Chay on 23/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Growcalth-iOS/Growcalth_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Growcalth_iOSApp.swift
+//  Growcalth-iOS
+//
+//  Created by Tristan Chay on 23/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Growcalth_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
