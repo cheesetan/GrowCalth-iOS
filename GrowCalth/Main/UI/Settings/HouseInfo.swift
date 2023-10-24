8c29c852cfa624dfa54a0de878a9b74b09ec1fb8
Tristan <tristanchay123@gmail.com>
2023-10-24 16:46:30 +0800

    renamed some things and completed whateevr i could in settings

---
+++ GrowCalth/Main/UI/Settings/HouseInfo.swift
@@ -0,0 +1,18 @@
+//
+//  HouseInfo.swift
+//  GrowCalth
+//
+//  Created by Tristan Chay on 24/10/23.
+//
+
+import SwiftUI
+
+struct HouseInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HouseInfo()
+}
