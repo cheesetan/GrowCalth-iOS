d7015cb030a56c27fbd9705d84d211d1cefc4ccc
Tristan <tristanchay123@gmail.com>
2023-10-24 12:18:35 +0800

    working on healthinfo

---
+++ Growcalth-iOS/Main/Managers/HealthInfoManager.swift
@@ -0,0 +1,55 @@
+//
+//  HealhInfoManager.swift
+//  Growcalth-iOS
+//
+//  Created by Tristan Chay on 24/10/23.
+//
+
+import SwiftUI
+
+import Foundation
+import SwiftUI
+
+struct HealthInfoItem: Codable, Identifiable {
+    var id = UUID()
+    var text: String
+}
+
+class HealthInfoManager: ObservableObject {
+    static let shared: HealthInfoManager = .init()
+    
+    @Published var healthinfos: [HealthInfoItem] = [] {
+        didSet {
+            save()
+        }
+    }
+        
+    init() {
+        load()
+    }
+    
+    func getArchiveURL() -> URL {
+        let plistName = "healthinfo.plist"
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        return documentsDirectory.appendingPathComponent(plistName)
+    }
+    
+    func save() {
+        let archiveURL = getArchiveURL()
+        let propertyListEncoder = PropertyListEncoder()
+        let encodedHealthinfos = try? propertyListEncoder.encode(healthinfos)
+        try? encodedHealthinfos?.write(to: archiveURL, options: .noFileProtection)
+    }
+    
+    func load() {
+        let archiveURL = getArchiveURL()
+        let propertyListDecoder = PropertyListDecoder()
+                
+        if let retrievedHealthInfoData = try? Data(contentsOf: archiveURL),
+            let healthinfosDecoded = try? propertyListDecoder.decode([HealthInfoItem].self, from: retrievedHealthInfoData) {
+            healthinfos = healthinfosDecoded
+        }
+    }
+}
+
